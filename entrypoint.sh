aafba4e8a32c5a1ed7d711dcfd81ccb328902aa8
Vladyslav <stahnukvlad21@gmail.com>
2026-03-13 16:03:06 +0200

    Fix entrypoint.sh, fix celery command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set e
 echo "Running migrations"
 python -m alembic upgrade head
 
-celery -A app worker --loglevel=info --pool=solo --concurrency=1 &
+celery -A app.celery_config worker --loglevel=info --pool=solo --concurrency=1 &
 
 echo "Start web server"
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
